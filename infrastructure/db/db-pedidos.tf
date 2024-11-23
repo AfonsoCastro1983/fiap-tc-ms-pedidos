1151b18a897bfb8ef4f5374a8605e66d20eb01c2
AfonsoCastro1983 <afonso.castro@gmail.com>
2024-11-23 11:20:49 -0400

    Criação do banco de dados

---
+++ infrastructure/db/db-pedidos.tf
@@ -0,0 +1,33 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_dynamodb_table" "pedido_table" {
+  name           = "mspedido"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "id"
+
+  attribute {
+    name = "id"
+    type = "N" # Número
+  }
+
+  # Configuração opcional para facilitar consultas secundárias (Index)
+  global_secondary_index {
+    name            = "StatusIndex"
+    hash_key        = "status"
+    projection_type = "ALL"
+
+    # Capacidade para índices globais, ajustada automaticamente em PAY_PER_REQUEST
+  }
+
+  attribute {
+    name = "status"
+    type = "S" # String
+  }
+
+  tags = {
+    Application = "FIAP-TechChallenge"
+    Name = "dynamodb-fiap-ms-pedido"
+  }
+}
