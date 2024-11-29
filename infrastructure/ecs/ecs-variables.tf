ec8c896559f18e084c34141d8ea8567459710bf9
AfonsoCastro1983 <afonso.castro@gmail.com>
2024-11-28 20:39:30 -0400

    Correções criação load balancer

---
+++ infrastructure/ecs/ecs-variables.tf
@@ -0,0 +1,9 @@
+variable "aws_acces_key" {
+  description = "AWS Access Key"
+  type        = string
+}
+
+variable "aws_secret_access_key" {
+  description = "AWS Secret Access Key"
+  type        = string
+}
